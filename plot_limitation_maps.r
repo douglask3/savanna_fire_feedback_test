6c28b9accca56885ddf83412f3e99f8d71082ad8
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-06 14:45:55 +0000

    for plotting ensembler for all precip and drought combinations

--- plot_limitation_maps.r
+++ plot_limitation_maps.r
@@ -7,6 +7,10 @@ graphics.off()
 limits = c(1, 10, 20, 30, 40, 50, 60, 70)
 cols = c("white", "#AAAA00", "#003300")
 
+
+limits = c(0.2, 0.4, 0.6, 0.8)
+cols = c("FF", "BB","88", "44", "00")
+
 dlimits = c(-40, -20, -10, -5, -2, -1, 1, 2, 5, 10, 20, 40)
 dcols = c("#330000", "#DD0033", "#FF33FF", "white", "#FFFF00", "#00FF00", "#003300")
 items = c(2:3, 5:6)
@@ -15,22 +19,20 @@ items = c(2:3, 5:6)
 ## load and analyes  			      ##
 ########################################
 
-out = makeOrLoadEnsembles()
-lim = lapply(items, function(i) 1-selectOutput(out, i)[[1]])
+plot4pr_droughts <- function(pr_dataset, drought_var) {
+    out = makeOrLoadEnsembles(pr_dataset = pr_dataset, drought_var = drought_var)
+    lim = lapply(items, function(i) 1-selectOutput(out, i)[[1]])
+    sen = lapply(items, function(i)  tail(selectOutput(out, i), 1)[[1]])
 
-sen = lapply(items, function(i)  tail(selectOutput(out, i), 1)[[1]])
 
-
-limits = c(0.2, 0.4, 0.6, 0.8)
-cols = c("FF", "BB","88", "44", "00")
 	
-xy = xyFromCell(lim[[1]], 1:length(lim[[1]]))
-mlim = lim
+    xy = xyFromCell(lim[[1]], 1:length(lim[[1]]))
+    mlim = lim
 
-nlim = mlim[[1]] + mlim[[2]] + mlim[[3]] + mlim[[4]]
-nlim = lapply(mlim, '/', nlim)
+    nlim = mlim[[1]] + mlim[[2]] + mlim[[3]] + mlim[[4]]
+    nlim = lapply(mlim, '/', nlim)
 
-plotMap <- function(x, fname = '', normalise = FALSE) {
+    plotMap <- function(x, fname = '', normalise = FALSE) {
 	fname = paste('figs/limPlot', fname, '.png', sep = '')
 	png(fname, height = 6, width = 12, res = 300, unit = 'in')
 	print(fname)
@@ -47,17 +49,17 @@ plotMap <- function(x, fname = '', normalise = FALSE) {
 	x[c(2, 4)] = lapply(x[c(2, 4)], function(i) {i[1:20, 345:420][is.na(i[1:20, 345:420])] = quantile(i, 0.1); i})
 	
 	plotFun <- function(i, cols, title) {
-		plot_raster_from_raster(i, cols = cols, limits = seq(0.0, 1.0, 0.1), add_legend=FALSE, interior = FALSE)
-		addStandardLegend(i, rev(seq(0.0, 1.0, 0.1) * 100) , cols, units = '')
-		mtext(side = 3, adj = 0.2, title, line = -1.15, cex = 1.5)
+	    plot_raster_from_raster(i, cols = cols, limits = seq(0.0, 1.0, 0.1), add_legend=FALSE, interior = FALSE)
+            addStandardLegend(i, rev(seq(0.0, 1.0, 0.1) * 100) , cols, units = '')
+	    mtext(side = 3, adj = 0.2, title, line = -1.15, cex = 1.5)
 	}
 	
 	
 	mapply(plotFun, x, 
-			cols = list(c("white", '#00FFFF', '#000011'),
-			            c("white", '#888800', "#001100"),
-						c("white", "#FF00FF", "#110000"),
-						c("white", "black")), plot_title)
+	       cols = list(c("white", '#00FFFF', '#000011'),
+			   c("white", '#888800', "#001100"),
+			   c("white", "#FF00FF", "#110000"),
+			  c("white", "black")), plot_title)
 	
 	
 	#2.5:4.5
@@ -87,11 +89,18 @@ plotMaps <- function(x, fname, ...) {
 		plotMap(x, fname, ...)
 	}
 	plotStuff(1, 'mean', mean, mean)
-	mapply(plotStuff, 1:4, paste('max', 1:4), MoreArgs = list( function(i) max(i),  function(i) min(i)))
-	mapply(plotStuff, 1:4, paste('min', 1:4), MoreArgs = list( function(i) min(i),  function(i) max(i)))
+	#mapply(plotStuff, 1:4, paste('max', 1:4), MoreArgs = list( function(i) max(i),  function(i) min(i)))
+	#mapply(plotStuff, 1:4, paste('min', 1:4), MoreArgs = list( function(i) min(i),  function(i) max(i)))
+
+    }
+    
+    fname = paste0(pr_dataset, '-', drought_var, '-')
+    plotMaps(mlim, paste0(fname, 'standard'))
+    #plotMaps(nlim, paste0(fname, 'potential'))
+    plotMaps(sen, paste0(fname, 'sensitivity'), TRUE)
 	
 }
 
-plotMaps(mlim, 'standard')
-plotMaps(nlim, 'potential')
-plotMaps(sen, 'sensitivity', TRUE)
+#plot4pr_droughts('MSWEP', 'MADD')
+runAll_pr_droughts(plot4pr_droughts)
+
