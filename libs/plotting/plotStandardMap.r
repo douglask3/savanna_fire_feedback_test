53a9cd71b69d67e020b7d63ac78277763d95bc81
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-06 17:04:50 +0000

    drastic tidy up of tree cover and experiments plots

--- libs/plotting/plotStandardMap.r
+++ libs/plotting/plotStandardMap.r
@@ -2,9 +2,9 @@ plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE,...) {
 	
     plot_raster_from_raster(x, limits = limits, cols = cols,x_range = c(-120, 160),y_range = c(-30, 30),
                             transpose = FALSE, srt = 0,
-                            plot_loc = c(0.35,0.83,0.01,0.04),
+                            plot_loc = c(0.35,0.83,0.1,0.4),
                             quick = TRUE, add_legend = add_legend, ...)
-    mtext(txt,side = 3, line = -1.5)
+    mtext(txt, side = 3, line = 1)
 }
 
 plotStandardMap.sd <- function(x, sc = 1, ...)
