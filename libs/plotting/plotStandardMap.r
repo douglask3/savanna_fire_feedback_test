75a657b0b011960d30a57b6b7c011c5629497a57
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-07 18:01:21 +0000

    plotting inputs for main run

--- libs/plotting/plotStandardMap.r
+++ libs/plotting/plotStandardMap.r
@@ -1,11 +1,15 @@
-plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE,...) {
+plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE, mtext_line = 1,
+                           ...) {
 	
     plot_raster_from_raster(x, limits = limits, cols = cols,
                             x_range = c(-120, 160),y_range = c(-30, 30),
                             transpose = FALSE, srt = 0,
                             plot_loc = c(0.35,0.83,0.1,0.4),
-                            quick = TRUE, add_legend = add_legend, ...)
-    mtext(txt, side = 3, line = 1)
+                            quick = TRUE, add_legend = FALSE, ...)
+    mtext(txt, side = 3, line = mtext_line)
+    
+    if (add_legend) addStandardLegend(x, limits, cols, 
+                                       plot_loc = c(0.1, 0.9, -0.15, -0.05), ...)
 }
 
 plotStandardMap.sd <- function(x, sc = 1, ...)
