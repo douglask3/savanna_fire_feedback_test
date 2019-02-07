3b56428efa9043b26462d4f62af9e34a31575ff2
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-07 16:06:58 +0000

    tidying

--- libs/plotting/plotStandardMap.r
+++ libs/plotting/plotStandardMap.r
@@ -1,6 +1,7 @@
 plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE,...) {
 	
-    plot_raster_from_raster(x, limits = limits, cols = cols,x_range = c(-120, 160),y_range = c(-30, 30),
+    plot_raster_from_raster(x, limits = limits, cols = cols,
+                            x_range = c(-120, 160),y_range = c(-30, 30),
                             transpose = FALSE, srt = 0,
                             plot_loc = c(0.35,0.83,0.1,0.4),
                             quick = TRUE, add_legend = add_legend, ...)
@@ -8,5 +9,6 @@ plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE,...) {
 }
 
 plotStandardMap.sd <- function(x, sc = 1, ...)
-	plotStandardMap(mean(x) * sc, 
-					e = sd.raster(x), ePatternRes = 35, ePatternThick = 0.4, limits_error = c(0.1, 0.25),...)
+	plotStandardMap(mean(x) * sc, e = sd.raster(x),
+                        ePatternRes = 35, ePatternThick = 0.4, 
+                        limits_error = c(0.1, 0.25),...)
