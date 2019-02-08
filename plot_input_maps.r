1ad532508348a2adc37c95754925ca187e55c015
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-08 10:13:45 +0000

    plotting pr and dr seperate inputs

--- plot_input_maps.r
+++ plot_input_maps.r
@@ -18,8 +18,7 @@ plot_inputs_figure <- function() {
 
     dat = selectNotNone(dats, plot_vars)
     dat = mapply(scaleFun, dat, scaling)
-
-
+   
     plotMap <- function(x, box, ...) {
         if (is.raster(x)) {
             plotStandardMap(x, ..., add_legend = TRUE, mtext_line = -1.5)
@@ -34,8 +33,8 @@ plot_inputs_figure <- function() {
         } else plot.new()
     }
 
-    png('figs/inputs_map.png', height = 8, width = 16, units = 'in', res = 300)
-        par(mfrow = c(4, 3), mar = c(0, 0, 1, 0), oma = c(0, 0, 1, 0))    
+    png(figName, height = 8, width = 16, units = 'in', res = 300)
+        par(mfrow = plotDims, mar = c(0, 0, 1, 0), oma = c(0, 0, 1, 0))    
         mapply(plotMap, dat, box, limits = limits, cols = cols, names(plot_vars), units = units)
     dev.off.gitWatermark()
 }
@@ -44,10 +43,10 @@ plot_inputs_figure <- function() {
 ###############
 ## figure S1 ##
 ###############
-plot_vars = c(    c("A  Tree cover" = "TreeCover", "B  MAP" = "MAP_MSWEP", "C MAT" = "MAT"),
-                  c("D  Rainfall distribution" = "MADD_MSWEP", "E  Burnt are" = "BurntArea", "F  MxMTWM" = "MTWM"),
-                  c("G  Population Density" = "PopDen", "H  Urban Area" = "urban", "I  Cropland Area" = "crop"),
-                  c("NaN" = "None", "NaN" = "None", "J  Pasture Area" = "pas"))
+plot_vars = c("A  Tree cover" = "TreeCover", "B  MAP" = "MAP_MSWEP", "C MAT" = "MAT",
+              "D  Rainfall distribution" = "MADD_MSWEP", "E  Burnt are" = "BurntArea", "F  MxMTWM" = "MTWM",
+              "G  Population Density" = "PopDen", "H  Urban Area" = "urban", "I  Cropland Area" = "crop",
+              "NaN" = "None", "NaN" = "None", "J  Pasture Area" = "pas")
 
 units   = list('%', 'mm/yr', '~DEG~C', '', '%', '~DEG~C',
                "people/k~m2~", '%', '%', '', '', '%')
@@ -55,14 +54,14 @@ units   = list('%', 'mm/yr', '~DEG~C', '', '%', '~DEG~C',
 scaling = list(100/0.8, exp, 1, 1, 100, 1, 1, 100, 100, NaN, NaN, 100)
 
 
-pr_limits      = c(0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000)
+pr_limits      = c(100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000)
 drought_limits = seq(0, 1, 0.1)
 limits = list(c(10, 20, 30, 40, 50, 60, 70, 80, 90),
               pr_limits,
-              c(8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28),
+              c(8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30),
               drought_limits,
               c(0, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50),
-              c(12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34),
+              c(12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36),
               c(0.1, 1, 10, 100, 1000),
               c(0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1),
               seq(5, 60, 5),
@@ -73,7 +72,7 @@ limits = list(c(10, 20, 30, 40, 50, 60, 70, 80, 90),
 tas_colour = c('#5e4fa2', '#66c2a5', '#e6f598', '#fee08b', '#f46d43', '#9e0142')
 agr_colour = c('#fff7f3', '#fcc5c0', '#f768a1', '#ae017e', '#49006a', '#120017')
 prc_colour = c('#ffffd9', '#c7e9b4', '#41b6c4', '#225ea8', '#081d58', '#020d3a')
-drt_colour = c('#ffffd9', '#c7e9b4', '#41b6c4', '#225ea8', '#081d58', '#020d3a')
+drt_colour = c('#ffffe5', '#fee391', '#fe9929', '#cc4c02', '#662506', '#160601')
 
 cols = list(c("white" , "#AAAA00", "#003300"),
             prc_colour,
@@ -94,22 +93,30 @@ box = list(NULL, c('#0000AA' = 1),
           c('red' = 1,    'black'   = 2),
           c('black'   = 1), c('black'   = 1), NULL, NULL, c('black' = 1))                
 
-          
+plotDims = c(4,3)  
+figName = 'figs/inputs_map.png'  
+
+plot_inputs_figure()
 ###############
 ## figure S1 ##
 ###############
 
-plot_vars =  paste0(c("MAP", drought_vars), '_', rep(pr_datasets, each = 5))
+plot_vars =  paste0(rep(c("MAP", drought_vars), each = 4), '_', pr_datasets)
 
-names(plot_vars) = paste(LETTERS[1:16], '', c("MAP", drought_vars),rep(pr_datasets, each = 5))
+names(plot_vars) = paste(LETTERS[1:16], '', rep(c("MAP", drought_vars), each = 4), pr_datasets)
 
 
-units   = c(rep('mm/yr', 4), rep('', 20))
+units   = c(rep('mm/yr', 4), rep('', 16))
 
-scaling = c(rep(c(exp), 5), rep(1, 20))
+scaling = c(rep(c(exp), 4), rep(1, 16))
 
-limits = c(rep(list(pr_limits), 5), rep(list(drought_limits), 5))
+limits = c(rep(list(pr_limits), 4), rep(list(drought_limits), 16))
 
-col = c(rep(list(prc_colour), 5), rep(list(drt_colour), 20))
+cols = c(rep(list(prc_colour), 4), rep(list(drt_colour), 16))
 
-box = NULL      
+box = rep(list(NULL), 16)
+
+plotDims = c(5, 4)   
+figName = 'figs/inputs_pr_dr_map.png'
+
+plot_inputs_figure()   
