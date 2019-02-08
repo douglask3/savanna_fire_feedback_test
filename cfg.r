b9508980453e71770a83f936d3849a5aebfc5f5d
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-08 07:55:50 +0000

    cfg that works locally on windows machine

--- cfg.r
+++ cfg.r
@@ -28,7 +28,7 @@ setupProjectStructure(dirn = c("outputs", "data", "temp", "figs"))
 ens_dir = paste(outputs_dir, '/ensembles_noSW/', sep = '')
 makeDir(ens_dir)
 
-ensemble_no = round(seq(1,4200, length.out = 6) )
+ensemble_no = round(seq(1,4200, length.out = 51) )
 grab_cache_default = TRUE
 
 sourceAllLibs('libs/')
