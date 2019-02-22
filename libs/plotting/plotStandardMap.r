467aa4637f2b2116d5c8b01642d6b52037919b61
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-22 17:25:30 +0000

    tidyiong

--- libs/plotting/plotStandardMap.r
+++ libs/plotting/plotStandardMap.r
@@ -2,7 +2,7 @@ plotStandardMap <- function(x, limits, cols, txt = '', add_legend = FALSE, mtext
                            ...) {
 	
     plot_raster_from_raster(x, limits = limits, cols = cols,
-                            x_range = c(-120, 160),y_range = c(-30, 30),
+                            x_range = c(-120, 160), y_range = c(-30, 30),
                             transpose = FALSE, srt = 0,
                             plot_loc = c(0.35,0.83,0.1,0.4),
                             quick = TRUE, add_legend = FALSE, ...)
