08828f38c79ac942fa24fabd18e83200a130e808
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-05-02 18:00:49 +0100

    resizing plots

--- plot_ensemble.r
+++ plot_ensemble.r
@@ -25,7 +25,7 @@ plotExps <- function(fname, ExpID, out, dout, plotFullModOnly = FALSE, obsOnly =
     p_rows = (length(ExpIDp)/2)
 	
     if (!plotFullModOnly && !obsOnly) {
-        png(fname[1], height = 5 * (p_rows + 1.6)/4.6, width = 8, unit = 'in', res = 300)	
+        png(fname[1], height =(4.75/6.5) * 5 * (p_rows + 1.6)/4.6, width = 4.75, unit = 'in', res = 300)	
         lmat = rbind(1:2, 3,t(matrix(ExpIDp, nrow = 2)) + 3, max(ExpIDp, na.rm = TRUE) + 4)
         lmat[is.na(lmat)] = 0.0
 	
@@ -36,20 +36,24 @@ plotExps <- function(fname, ExpID, out, dout, plotFullModOnly = FALSE, obsOnly =
     if (plotFullModOnly) modTitle = '' else modTitle = 'reconstructed'
 	
 	if (!plotFullModOnly)
-        plotStandardMap(dats[[1]][['TreeCover']] * 100/0.8, limits = limits, cols = cols, obsTitle)
+        plotStandardMap(dats[[1]][['TreeCover']] * 100/0.8, limits = limits, cols = cols, obsTitle, mtext_line = 0.5)
 	if (obsOnly) return()
-    plotStandardMap.sd(out[[1]], 100, limits = limits, cols = cols, modTitle)
+    plotStandardMap.sd(out[[1]], 100, limits = limits, cols = cols, modTitle, mtext_line = 0.5)
     if (plotFullModOnly) return()
     par(mar = c(0.5, 0, 0, 0))
-	addStandardLegend(out[[1]], limits, cols, add = FALSE, maxLab = '100')
+	addStandardLegend(out[[1]], limits, cols, add = FALSE, maxLab = '100',
+                          plot_loc = c(0.15, 0.85, 0.7, 0.9))
     par(mar = c(0, 0, 1.5, 0))
 
 	#mtext('Impact of ...', side = 1, line = 0, exp = NA)
 	mapply(plotStandardMap.sd, dout[(ExpID - 1)], txt = expNames[ExpID],
-               MoreArgs = list(100, limits = dlimits, cols = dcols))
+               MoreArgs = list(100, limits = dlimits, cols = dcols, 
+               limit_error = c(0.05, 0.1, 0.25), ePatternThick = 0.4,
+               mtext_line = 0.5))
 
         par(mar = rep(0,4))
-	    addStandardLegend(dout[[1]], dlimits, dcols, add = FALSE, extend_max = TRUE)
+	    addStandardLegend(dout[[1]], dlimits, dcols, add = FALSE, extend_max = TRUE,
+                              plot_loc = c(0.25, 0.75, 0.7, 0.9))
         par(mar = c(0, 0, 1.5, 0))
     dev.off()#.gitWatermark()
 
@@ -108,7 +112,7 @@ PlotAllExperiments <- function(pr_dataset, drought_var, ..., plotFullModOnly = F
 #PlotAllExperiments(pr_dataset = 'CMORPH', drought_var = 'MConc')
 #PlotAllExperiments(pr_dataset = 'CMORPH', drought_var = 'MDDM')
 
-#PlotAllExperiments(pr_dataset = 'MSWEP', drought_var = 'MADD')
+PlotAllExperiments(pr_dataset = 'MSWEP', drought_var = 'MADD')
 
 lmat = c(1, 0, 0, 0)
 lmat = rbind(lmat, matrix(2:17, ncol = 4))
