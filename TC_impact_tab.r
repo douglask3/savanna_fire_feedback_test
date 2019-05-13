67ea76e80132d96dbfc3e372ff04dd7a2ccf23d7
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-05-13 11:13:26 +0100

    running impact for everythig, buyt in specific order

--- TC_impact_tab.r
+++ TC_impact_tab.r
@@ -1,13 +1,14 @@
 source("cfg.r")
 graphics.off()
 
+expIDS = c(2, 3, 14, 13, 5, 6, 7, 8, 9, 10, 11, 12)
 
-TC_impact_tab <- function(pr_dataset, drought_var, ExpID = 5:12, ...) {
+TC_impact_tab <- function(pr_dataset, drought_var, ExpID = NULL, ...) {
     print(pr_dataset)
     print(drought_var)
     out = makeOrLoadEnsembles(pr_dataset = pr_dataset, drought_var = drought_var, ...)
     control = out[[1]]
-    Toff = out[ExpID]
+    if (!is.null(ExpID)) Toff = out[ExpID] else Toff = out
 
     extractTC <- function(i) layer.apply(i, function(i) i[[1]])
 
@@ -35,7 +36,13 @@ TC_impact_tab <- function(pr_dataset, drought_var, ExpID = 5:12, ...) {
 outs = runAll_pr_droughts(TC_impact_tab)
 
 outs_out = c()
-for (i in outs) for (j in i) outs_out = rbind(outs_out, j)
+for (i in outs) for (j in i) {
+    j = j[, expIDS]
+    j[2, ] = j[2,]/j[1,]
+    outs_out = cbind(outs_out, as.vector(j))
+    colnames(outs_out) = c(head(colnames(outs_out), -1), 
+                           strsplit(rownames(j)[1], '_mean'))
+}
 
-colnames(outs_out) = expNames[5:12]
+rownames(outs_out) = paste(rep(expNames[expIDS], each = 2), c('mean', 'sd'))
 write.csv(outs_out, file = 'outputs/TCimpactTab.csv')
