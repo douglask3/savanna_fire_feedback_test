5ddf2adaf9bc254bd0f9238956ed97574d99e1d4
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-10-24 09:19:27 +0100

    tidying

--- libs/openJulesExperimentSet.r
+++ libs/openJulesExperimentSet.r
@@ -5,17 +5,22 @@ openJulesExperimentSet <- function(name, JULES_control, JULES_experiments,
 	load(temp_file)
     } else {        
 	Jules_TC_control = openJulesTree(JULES_control, JULES_dir, ...)
-	Jules_PFTs       = openJulesTree(JULES_control, JULES_dir, splitPFTs = TRUE, ...)       
-	Jules_fire       = layer.apply(JULES_experiments, openJulesTree, JULES_dir, 1,
+	Jules_PFTs       = openJulesTree(JULES_control, JULES_dir, splitPFTs = TRUE, ...) 
+
+    Jules_BA_control = openJulesTree(JULES_control, JULES_dir, 1,
                                  "burnt_area_gb", yrs = yrs) *   60 * 60 * 24 * 365 
-	Jules_fire       = raster::resample(Jules_fire, Jules_TC_control)
+	Jules_BA_exp     = layer.apply(JULES_experiments, openJulesTree, JULES_dir, 1,
+                                 "burnt_area_gb", yrs = yrs) *   60 * 60 * 24 * 365 
+                         
+	Jules_BA_control = raster::resample(Jules_BA_control, Jules_TC_control)
+	Jules_BA_exp     = raster::resample(Jules_BA_exp    , Jules_TC_control)
 	Jules_TC_exp     = layer.apply(JULES_experiments, openJulesTree, JULES_dir, ...)
 		
 	Jules_dout = (Jules_TC_control - Jules_TC_exp)/Jules_TC_control
 	Jules_dout =  squeeze(Jules_dout, 500)
 	
-	save(Jules_TC_control, Jules_fire, Jules_TC_exp, Jules_dout, Jules_PFTs,
+	save(Jules_TC_control, Jules_BA_control, Jules_BA_exp, Jules_TC_exp, Jules_dout, Jules_PFTs,
              Jules_PFTs, file = temp_file)
     }
-    return(list(Jules_TC_control, Jules_fire, Jules_TC_exp, Jules_dout, Jules_PFTs))
+    return(list(Jules_TC_control, Jules_TC_exp, Jules_BA_control, Jules_BA_exp, Jules_dout, Jules_PFTs))
 }
