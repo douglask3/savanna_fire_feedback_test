f32acb0eaa834156f0ee8b163c8868e805d73698
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-05-11 14:17:33 +0100

    functions for converting from normal to pacific centric

--- libs/convert_pacific_centric_2_regular.r
+++ libs/convert_pacific_centric_2_regular.r
@@ -14,3 +14,24 @@ convert_pacific_centric_2_regular <- function(dat, tempWrite = FALSE) {
     if (tempWrite) dat = writeRaster(dat, file = memSafeFile())
     return(dat)
 }
+
+convert_regular_2_pacific_centric <- function(dat, tempWrite = FALSE) {
+    if (xmax(dat) > 300) return(dat)
+    dat_names = names(dat)
+    index = 1:length(values(dat[[1]]))
+
+    xyz = cbind(xyFromCell(dat,index), values(dat))
+    x = xyz[, 1]
+    
+    test = x < 0
+
+    x[test] = x[test] + 360
+
+    xyz[,1] = x
+    dat = rasterFromXYZ(xyz, crs = projection(dat))
+
+    names(dat) = dat_names
+
+    if (tempWrite) dat = writeRaster(dat, file = memSafeFile())
+    return(dat)
+}
