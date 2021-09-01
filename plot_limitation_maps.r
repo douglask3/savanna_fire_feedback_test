c6d85d0e8db832cc0e26fa1ce2b76cf5e4a7679b
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-09-01 14:56:20 +0100

    adding sperate legend for energy controls and tidying up 4way labels

--- plot_limitation_maps.r
+++ plot_limitation_maps.r
@@ -9,11 +9,14 @@ limits4 = c(0.33, 0.67)
 cols4 = c("FF", "BB","88", "44", "00")
 cols4 = c("FF", "88", "00")
 
-col4Labs = list(c('S', 's', ''), c('E', 'e', ''),  c('P', 'p', ''), c('T', 't', ''))
+col4Labs = list(c('S', 's', ''), c('L', 'l', ''),  c('P', 'p', ''), c('T', 't', ''))
 
-limits = list(c(1, 2, 5, 10, 25, 50, 75, 90, 95, 98, 99),
-              c(1, 2, 5, 10, 20, 30, 40, 50, 60),
-              c(1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90))
+limits = list(list(c(1, 2, 5, 10, 25, 50, 75, 90, 95, 98, 99),
+                   c(99, 99.5, 99.8, 99.9, 99.99)),
+              list(c(1, 2, 5, 10, 20, 30, 40, 50, 60),
+                   c(1, 2, 5, 10, 20, 30, 40, 50, 60)/100),
+              list(c(1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90),
+                   c(0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1)))
 limits2 = c(0.2, 0.5, 1, 2,5, 10, 20)
 cols = c("white", "#AAAA00", "#003300")
 cols = c('#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476',
@@ -33,7 +36,7 @@ limTypes = c("Standard limitation" = "standard",
 normalies = c(T, T, T, F)
 
 ########################################
-## load and analyes  			      ##
+## load and analyes  		      ##
 ########################################
 
 plotMap <- function(limType, limits, title = '',
@@ -47,7 +50,7 @@ plotMap <- function(limType, limits, title = '',
     }
    
     fname = paste('figs/limPlot', fname, '-', limType, '.png', sep = '')
-    png(fname, height = 4.33, width = 7.2, res = 300, unit = 'in')
+    png(fname, height = 4.33*5/4.5, width = 7.2, res = 300, unit = 'in')
 	print(fname)
         #if (limType == "standardxx") {
         #    heights = c(1,1,1,0.5, 1)
@@ -57,10 +60,10 @@ plotMap <- function(limType, limits, title = '',
         #    r1 = 10:12; r2 = c(13, 13, 14)
         #}
         
-	layout(rbind(1:2, 3:4, 5:6, 7:8, 9), heights = c(1,1,1,1,0.5))
+	layout(rbind(1:2, 3:4, 5:6, 7, 8:9, 10), heights = c(1,1,1,0.5, 1,0.5))
 	par(mar = c(0.5, 0, 0.5, 0), oma = c(0,0,2.3, 0))	
 	
-	plotFun <- function(lim, title, addTotTitle, normalise) {
+	plotFun <- function(lim, title, addTotTitle, normalise, limits) {
             limx = lapply(c(3,7), function(x) lim[[x]]*100)
             limx = lapply(limx, function(r) {r[r>9E9] = NaN; r})
             limx = lapply(limx, function(r) {r[lims[[4]][[1]]>9E9] = NaN; r})
@@ -73,8 +76,9 @@ plotMap <- function(limType, limits, title = '',
                 limp = lapply(limx, '/', mx)
             } else limp = limx
             limp = c(limp, limp[[2]] - limp[[1]])
-	    FUN <- function(r, limitsi = limits)
+	    FUN <- function(r, limitsi = limits[[1]])
                 plotStandardMap(r, cols = cols, limits = limitsi)
+            
             FUN(limp[[1]], limits)
             #if (limType == "sensitivity") browser()
             mtext(side = 2, title, line = -1.5)
@@ -87,12 +91,20 @@ plotMap <- function(limType, limits, title = '',
             #if (addTotTitle) mtext(side = 3, 'Range%', line = 0)            
             return(limx)          
 	}	
-	if (limType == "standardX") index = 1:3 else index = 1:4
+	index = 1:3
+        
 	limsx = mapply(plotFun, lims[index], names(controls)[index],
-                      c(T, rep(F, length(lims)-1))[index],
-                       normalies[index], SIMPLIFY = FALSE)    
+                       c(T, rep(F, length(lims)-1))[index],
+                       normalies[index], MoreArgs = list(limits = limits[[1]]), 
+                       SIMPLIFY = FALSE) 
+        addStandardLegend(lims[[1]][[1]], limits[[1]] , cols, units = '', add = FALSE, 
+                          plot_loc = c(0.2, 0.8, 0.55, 0.7), maxLab = 100, srt = 45) 
+        if (limType != "standardX") {
+            plotFun(lims[[4]], names(controls)[4],
+                    F, T, limits = limits[[2]])
+        }
         title(title, outer = TRUE, line = 1)    
-        addStandardLegend(lims[[1]][[1]], limits , cols, units = '', add = FALSE, 
+        addStandardLegend(lims[[1]][[1]], limits[[2]] , cols, units = '', add = FALSE, 
                           plot_loc = c(0.2, 0.8, 0.55, 0.7), maxLab = 100, srt = 45)      
         #addStandardLegend(lims[[1]][[1]], limits2 , cols, units = '', add = FALSE, 
         #                  plot_loc = c(0.1, 0.9, 0.55, 0.7), extend_max = TRUE, srt = 45)
@@ -106,7 +118,7 @@ plotAllTypes <- function(...)
             MoreArgs = list(...), SIMPLIFY = FALSE)
 
 plotAllTypes(fname = '10-90', ids = c(3, 7), header = c('10%', '90%'))
-#limsxs = plotAllTypes(fname = '25-75', ids = c(4, 6), header = c('25%', '75%'))
+limsxs = plotAllTypes(fname = '25-75', ids = c(4, 6), header = c('25%', '75%'))
 test = is.na(limsxs[[1]][[3]][[2]]) & !is.na(limsxs[[1]][[1]][[1]])
 limsxs[[1]][[3]][[2]][test] = 0.0
 
