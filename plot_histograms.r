89d9b61978699d231dce06fcab1e90f9c67fab6d
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-09-17 14:35:52 +0100

    Âadding biome type on observed histogram plot

--- plot_histograms.r
+++ plot_histograms.r
@@ -10,6 +10,9 @@ PostDir = "data/sampled_posterior/attempt15/"
 
 obsFile = 'data/driving_Data/TreeCover.nc'
 
+biomes = round(raster::resample(raster('data/biomAssigned.nc'),
+                          raster('data/driving_Data/TROPICS/TreeCover.nc')))
+
 conID  = "control"
 expIDs = c("Burnt\narea" = "noFire", "Heat\nStress" = "noTasMort","Wind" = "noWind",
             "Rainfall\ndistribution" = "noDrought", "Population\ndensity" = "noPop",
@@ -61,15 +64,44 @@ obs[obs > 1] = 1
 ctr = openDats(conID, 'Simulated')
 dev.off()
 
-png("figs/expHistogrammed.png", height = 12, width = 7.2, units = 'in', res = 300)
-par(mfrow = c(5, 2), mar = rep(0.5, 4), oma = c(4, 0, 0, 0))
-plot(range(bins)*100, c(0, 1), type = 'n', xlim = c(20, 100), axes = FALSE)
+png("figs/expHistogrammed.png", height = 12*6/5, width = 7.2, units = 'in', res = 300)
 
-chist =  hist(obs, breaks = bins, plot = FALSE)$counts
-chist = chist/max(chist[-(1:20)])
-chist = chist * ctr[1, 50] / chist[50]
-polygonDist(xs*100, chist, col = "black", border = NA)
+par(mfrow = c(5, 2), mar = rep(0.5, 4), oma = c(4, 0, 0, 0))
+
+plot(range(bins)*100, c(0, 1), type = 'n', xlim = c(20, 100), axes = FALSE,
+    xlab = '', ylab = '')
+
+polyArea <- function(x)
+    polygon(65 + x * 25 * c(-0.5, -0.5, 1.2, 1.2), c(0, 1, 1, 0),
+            border = NA, col = make.transparent("black", 0.99))
+
+lapply(seq(0, 1, by = 0.01), polyArea)
+text(x = 70, y = 0.67, 'Tree cover\ngap', font = 2, cex = 1.3)
+
+byBiome <- function(biome, r) {
+    if (!is.na(biome)) r = r[biomes == biome]
+    chist =  hist(r, breaks = bins, plot = FALSE)$counts
+}
+chist = sapply(c(NaN, 2:6, 8), byBiome, obs)
+chist[,1] = chist[,2]
+Mchist = max(chist[-(1:20), 1])
+chist = chist/Mchist
+chist = chist * ctr[1, 50] / chist[50, 1]
+
+polyD <- function(i, col) polygonDist(xs*100, chist[,i], col = col, border = NA)
+
+polyD(1, "black")
+
+cols = c("Wet Forests" = "#7570b3", "Dry Forests" = "#d95f02", "Savanna" = "#66c2a5", 
+          "Med." = "#8da0cb", "Summergreen" = "#1b9e77", 
+            "Desert" = "#fc8d62")  
+legend(x = 30, y = 1, names(cols)[-4], pch = 15, col = cols[-4], bty = 'n')
+#browser()
+for (i in 2:6) chist[,i] = apply(chist[,i:7], 1, sum)
+mapply(polyD, 2:7, cols)
+
 mtext(side = 3, adj = 0.9, line = -1, 'Observed', padj = 1)
+chist = chist[,2]
 ctr = openDats(conID, 'Simulated', chist = chist)
 mapply(openDats, expIDs, names(expIDs), axisT = c(rep(F, 6), T, T), MoreArgs = list(ctr = ctr))
 
