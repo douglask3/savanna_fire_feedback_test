834dad9e3c39a3612b4d413ff3949fdf17d90e35
Douglas Kelley <douglas.i.kelley@gmail.com>
2022-02-09 17:35:31 +0000

    running for all sites at once. Needs a lot of tidying

--- leafSize/leafsize_controls.r
+++ leafSize/leafsize_controls.r
@@ -7,134 +7,156 @@ library(rstan)
 source("libs/return_multiple_from_functions.r")
 
 grab_cache = TRUE
-filename = "data/leafsize_datasample.csv"
-nchains = 10
+filename ="data/leafsize_datasample.csv"
+nchains = 3
 nwarmup = 250
-niter = 10000
+niter = 1000
 nrefresh = 250
 
 ## for for sites with more than this number of samples
-nsiteMin = 200
+nsiteMin = 1
+
+
+logistic <- function(x, x0 = 0, k = 1) 
+  1/(1 + exp(-k*(x - x0)))
+
 
 ###############
 ## open      ##
 ###############
-dat = read.csv(filename)
+#dat =  data_global#evergreen_dat#read.csv(filename)
+dat =  read.csv(filename)
+dat = dat[order(dat[,'site.f']),]
+
+sites = dat[['site.f']]
+siteIDs = unique(sites)
 leafSizes = dat[['LS']]/10000
 climMeans = 10^dat[['pmin']]
-sites = dat[['site.f']]
+climMeans = sapply(siteIDs, function(i) climMeans[which(sites==i)[1]])
+sites = sapply(sites, function(i) which (siteIDs == i))
 
-###############
-## run       ##
-###############
-
-selectDat <- function(siteID) {
-    test = sites == siteID
-    leafSize = log(leafSizes[test])
-    climMean = log(climMeans[test][1])
+selectDat <- function(siteID = NaN) {
+    if (!is.na(siteID)) {
+        browser()
+        test = sites == siteID
+        leafSizes = leafSizes[test]
+        climMeans  = climMean[test]
+    }
+    
+    leafSize = log(leafSizes)
+    climMean = log(climMeans)
     return(list(leafSize, climMean))
 }
 
 runBayesian <- function(file, data, init, ...)  {
-    #data[[2]] = data[[2]]/(1+exp(0.3*(data[[2]]+5)))
-    #browser()
-    fit = stan(file = file, data = data,
-               chains = nchains,  warmup = nwarmup, iter = niter, cores = 1, refresh = 250,
-               init = rep(list(init), nchains),
-               control = list(max_treedepth = 10, adapt_delta = 0.95))
+  fit = stan(file = file, data = data,
+             chains = nchains,  warmup = nwarmup, iter = niter, cores = 3, refresh = 250,
+             init = rep(list(init), nchains),
+             control = list(max_treedepth = 10, adapt_delta = 0.95))
 }
-runBayesian.prescribedClim <- function(leafSize, climMean) 
-    runBayesian("leafSize/prescribedClim.stan", 
-                list(n = length(leafSize), LS = leafSize, cMu = climMean),
-                list(lsMu = mean(leafSize), lsSigma = sd(leafSize), climSd = 1))
 
+runBayesian.prescribedClim <- function(leafSize, climMean) {
+  
+  runBayesian("leafSize/prescribedClim.stan", 
+              list(nl = length(leafSize), ns =length(climMean), 
+                   LS = leafSize, siteIDs = sites, cMu = climMean, #
+                   LSmean = mean(leafSize), LSsd = sd(leafSize), climsd = 1),
+              list(lsMu = mean(leafSize), lsSigma = sd(leafSize), climSigma = 1))
+}
 
 runBayesian.varyingClim <- function(leafSize, climMean) 
-    runBayesian("leafSize/varyingClim.stan", 
-                list(n = length(leafSize), LS = leafSize),
-                list(lsMu = mean(leafSize), lsSigma = sd(leafSize),
-                     cMu = climMean, climSd = 1))
-
+  runBayesian("varyingClim.stan", 
+              list(n = length(leafSize), LS = leafSize),
+              list(lsMu = mean(leafSize), lsSigma = sd(leafSize),
+                   cMu = climMean, climSd = 1))
 
 run4Site <- function(siteID, FUN = runBayesian.prescribedClim, tname = "precribed") {
-    ofile = paste('outputs/rstan_contraints_for_site', tname,   
-                  siteID, nchains, nwarmup, niter, '.csv', sep = '-')
-    if (file.exists(ofile) && grab_cache) return(ofile)
-       
-    c(leafSize, climMean) := selectDat(siteID)
-    fit = FUN(leafSize, climMean)
-    params = data.frame(rstan::extract(fit))
-    
-    write.csv(params, file = ofile)
-    return(ofile)
+  ofile = paste('outputs/rstan_contraints_for_site', tname,   
+                siteID, nchains, nwarmup, niter, '.csv', sep = '-')
+  if (file.exists(ofile) && grab_cache) return(ofile)
+  
+  c(leafSize, climMean) := selectDat(siteID)
+  #  dnorm(xmod, p['lsMu'], p['lsSigma'] * logistic(x, x0 
+  
+  fit = FUN(leafSize, climMean)
+  
+  params = data.frame(rstan::extract(fit))
+  browser()
+  write.csv(params, file = ofile)
+  return(ofile)
 } 
 
-               
-siteIDs = unique(sites)
-test = sapply(siteIDs, function(siteID) sum(sites == siteID) > nsiteMin)
-siteIDs = siteIDs[test][1:3]
-
-outPrescribed = lapply(siteIDs, run4Site)
-outVarying = lapply(siteIDs, run4Site, runBayesian.varyingClim, "varying")
-
-nplots = length(siteIDs)
-nrow = floor(sqrt(nplots))
-ncol = ceiling(nplots/nrow)
-
-
-par(mfcol = c(nplots, 2), mar = c(1.5, 0.1, 0.1, 0.1))
-
-logistic <- function(x, x0 = 0, k = 1) 
-    1/(1 + exp(-k*(x - x0)))
+#siteIDs = unique(sites)
+#test = sapply(siteIDs, function(siteID) sum(sites == siteID) > nsiteMin)
+#siteIDs = siteIDs[test]
+#
+outGlobal = run4Site(NaN)
+browser()
+#outPrescribed = lapply(siteIDs, run4Site)
 
 plotSite <- function(siteID, pfile) {
-    ps = read.csv(pfile)
-    ps = ps[sample(1:nrow(ps), 1000),]
-    
-    c(leafSize, climMean) := selectDat(siteID)
-    leafSize = log10(exp(leafSize))
-    ls = hist(leafSize, length(leafSize)/10, plot = FALSE)
-    
-    y = ls[['density']]/max(ls[['density']])
-    x = ls[['mids']]
-
-    xrange = range(c(x, log10(exp(climMean)+2)))
-    plot(x, y, type = 'h', axes = FALSE, xlim = xrange, ylim = c(0, 1))
-    axis(1, at = (-(20):20), 10^(-(20):20))
-
-    xmod = seq(-20, 20, 0.1)
-    xp = log10(exp(xmod))
-    addMod <- function(p) {
-        
-        lines(log10(exp(c(climMean, climMean))), c(0, 1), col = 'blue', lwd = 2, lty = 2)
-        if (!is.na(p['cMu'])) 
-            climMean = p['cMu']
-        
-        yclim = logistic(xmod, climMean, -p['climSigma'])
-        lines(xp, yclim, col = '#0000FF01', lwd = 2)
-        
-        ybio = dnorm(xmod, p['lsMu'], p['lsSigma'])
-        ybio = 0.67*ybio/max(ybio)
-        if (sample(c(T,F), 1, prob = c(0.1, 0.9)))
-            polygon(xp, ybio, border = NA, col = '#FF000005')   
-
-        1-sqrt(sum(ybio*ybio*yclim))/sqrt(sum(ybio*ybio))
+  rm(test,climImpact,climMean)
+  
+  ps = read.csv(pfile)
+  ps = ps[sample(1:nrow(ps), 1000),]
+  # browser()
+  c(leafSize, climMean) := selectDat(siteID)
+  leafSize = log10(exp(leafSize))
+  ls = hist(leafSize, length(leafSize)/10, plot = FALSE)
+  
+  y = ls[['density']]/max(ls[['density']])
+  x = ls[['mids']]
+  
+  xrange = range(c(x, log10(exp(climMean)+2)))
+  plot(x, y, type = 'h', axes = T, xlim = xrange, ylim = c(0, 1))
+  
+  axis(1, at = (-(20):20), 10^(-(20):20))
+  
+  xmod = seq(-20, 20, 0.1)
+  xp = log10(exp(xmod))
+  
+  addMod <- function(p) {
+    #browser()
+    if (!is.na(p['cMu'])) {
+      lines(c(climMean, climMean), c(0, 1), col = 'blue', lwd = 2, lty = 2)
+      climMean = p['cMu']
     }
-    climImpact = apply(ps, 1, addMod)
-    climImpact = round(quantile(100*climImpact, c(.05, 0.95)), 2)
-    mtext(side = 1, line = -5, adj = 0.1, paste0(climImpact[1], '-', climImpact[2], '%'))    
-    addHistLine <- function(x, y, ...) 
-        mapply(function(i, j, ...) lines(c(i, i), c(0, j), ...), x, y)
-
-    addHistLine(x, y)
-    lsMuRng = quantile(log10(exp(ps[['lsMu']])), c(0.1, 0.9))
-    points(lsMuRng, y = c(-0.0, -0.0), xpd = TRUE, pch = 19)
-    lines(lsMuRng, c(0, 0))
-    return(climImpact)
     
+    yclim = logistic(xmod, climMean, -p['climSigma'])
+    lines(xp, yclim, col = '#0000FF01', lwd = 2)
+    
+    ybio = dnorm(xmod, p['lsMu'], p['lsSigma'])
+    ybio = 0.67*ybio/max(ybio)
+    
+    if (sample(c(T,F), 1, prob = c(0.1, 0.9)))
+      polygon(xp, ybio, border = NA, col = '#FF000005')   
+    
+    1-sqrt(sum(ybio*ybio*yclim))/sqrt(sum(ybio*ybio))
+  }
+  
+  climImpact = apply(ps, 1, addMod)
+  
+  test <- cbind(climImpact,ps)
+  test[which(test$climImpact == max(test$climImpact)),'climSigma']
+  
+  climImpact = round(quantile(100*climImpact, c(.05, 0.95)), 2)
+  
+  
+  mtext(side = 1, line = -7, adj = 0.1, paste0(climImpact[1], '-', climImpact[2], '%'),col='blue')    
+  
+  addHistLine <- function(x, y, ...) 
+    mapply(function(i, j, ...) lines(c(i, i), c(0, j), ...), x, y)
+  
+  addHistLine(x, y)
+  
+  lsMuRng = quantile(log10(exp(ps[['lsMu']])), c(0.1, 0.9))
+  points(lsMuRng, y = c(-0.0, -0.0), xpd = TRUE, pch = 19)
+  lines(lsMuRng, c(0, 0))
+  
+  lssigma = quantile(ps[['lsMu']], c(0.1,0.5, 0.9))
+  
+  write.csv(cbind(test[which(test$climImpact == max(test$climImpact)),],siteID,climMean,lssigma[[1]], lssigma[[2]],lssigma[[3]]), file = paste('temp/max_for_site',  siteID, 'Prescribed_param.csv', sep = '-'))
+  
+  return(climImpact)
+  
 }
-mapply(plotSite, siteIDs, outPrescribed)
-
-mapply(plotSite, siteIDs, outVarying)
-
-
--- leafSize/prescribedClim.stan
+++ leafSize/prescribedClim.stan
@@ -5,9 +5,14 @@ functions{
   }
 }
 data{
-int<lower = 0> n; // number of observations
-vector[n] LS; // vector of observed leaf size
-real cMu; // climate mean constraint
+int<lower = 0> nl; // number of observations
+int<lower = 0> ns; // number of sites
+vector[nl] LS; // vector of observed leaf size
+int siteIDs[nl];
+vector[ns] cMu; // climate mean constraint
+real LSmean;
+real LSsd;
+real climsd;
 }
 
 //transformed data{
@@ -16,22 +21,32 @@ real cMu; // climate mean constraint
 
 parameters{
 real lsMu; //mean bio leaf size
-real<lower = 0> lsSigma; // sd bio leaf size
-real<lower = 0> climSigma; //sd climate constraint
+real<lower = 0.001> lsSigma; // sd bio leaf size
+real<lower = 0.001> climSigma; //sd climate constraint
 //real<lower = 0> sigma; // overall error
 }
 
 
 
 model{
-real nfact;
+vector[ns] nfact;
 real x;
+int ID;
 
-nfact = 0.0;
-for (i in 1:1000) 
-    nfact += customProb((i-500.0)/25.0, lsMu, lsSigma,  cMu, -climSigma);    
+lsMu ~ normal(LSmean, 1);
+lsSigma ~ lognormal(LSsd, 1);
+climSigma ~ lognormal(climsd, 1);
 
-for (i in 1:n)
-  target += customProb(LS[i], lsMu, lsSigma,  cMu, -climSigma) - nfact;
+for (i in 1:ns) {
+    nfact[i] = 0.0;
+    for (j in 1:100) {
+        nfact[i] += customProb((j-50.0)/2.50, lsMu, lsSigma,  cMu[i], -climSigma);  
+    }
+}
+
+for (i in 1:nl) {   
+    ID = siteIDs[i];
+    target += customProb(LS[i], lsMu, lsSigma,  cMu[ID], -climSigma) - nfact[ID];
+}
 }
 
