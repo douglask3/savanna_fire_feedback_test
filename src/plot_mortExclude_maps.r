27270ff99d227f408895bfe93781ac14fc449d8d
Douglas Kelley <douglas.i.kelley@gmail.com>
2022-12-16 20:45:11 +0000

    plotting withour rainfall and fire and without rainfall and humans

--- src/plot_mortExclude_maps.r
+++ src/plot_mortExclude_maps.r
@@ -9,20 +9,42 @@ cols = c('#ffffe5','#fff7bc','#fee391','#fec44f','#fe9929',
 
 limits = c(0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 40)
 
-summaryFileC = "model_summary-nEns-111.nc"
-summaryFileE = "model_summary-nEns-diff111.nc"
+summaryFileC = "model_summary-nEns-11"
+summaryFileE = "model_summary-nEns-diff11"
+
 PostDir = "data/sampled_posterior/attempt15"
 
 conID  = "control"
 expIDs = c("Burnt\narea" = "noFire", "Heat\nStress" = "noTasMort","Wind" = "noWind",
-            "Rainfall\ndistribution" = "noDrought", "Population\ndensity" = "noPop",
-            "Urban\narea" = "noUrban", "Cropland\narea" = "noCrop", "Pasture\narea" = "noPas")
+            "Rainfall\ndistribution" = "noDrought",  
+            "Population\ndensity" = "noPop",
+            "Urban\narea" = "noUrban", "Cropland\narea" = "noCrop", "Pasture\narea" = "noPas",
+            "Direct\nhuman impacts" = "noHumans", 
+            "Direct\nhuman impacts\n& Fire" = "noHumans_noFire")
 obsFile = 'data/driving_Data/TreeCover.nc'   
+
+
 
 #model_summary-nEns-diff101.nc
 openDat <- function(id, summaryFile) {
-    if (id == "noDrought" || id == "noPop") summaryFile = "model_summary-nEns-diff101.nc"
-    brick.NaN(paste0(PostDir, '/', id, '/', summaryFile),
+    if (id == "noDrought" || id == "noPop") summaryFile = "model_summary-nEns-diff101"
+    file = list.files(paste0(PostDir, '/', id, '/'))
+    file = file[grepl(summaryFile, file) | grepl('difffrom', file)]
+    if (length(file) > 1) {
+        fromtest = grepl('from', file)
+        if (any(fromtest)) file = file[fromtest]
+        #nens = sapply(file, function(i) tail(strsplit(i, 'diff')[[1]], 1))
+        if (length(file) > 1) {
+            test = substr(file, nchar(file)-7, nchar(file)-3) == 
+                    substr(summaryFile, nchar(summaryFile)-4, nchar(summaryFile))
+            
+            if(!any(test) || sum(test) > 1) browser()
+            file = file[test]
+        }
+    }
+    #if (id == "noDrought_noFire") browser()
+    print(paste(id, file, sep = ": "))
+    brick.NaN(paste0(PostDir, '/', id, '/', file),
               varname = "tree_cover_mean", layers = c(3, 7))
 }
 
@@ -30,10 +52,9 @@ control = 100*openDat(conID, summaryFileC)
 obs = raster::resample(raster(obsFile), control)
 obs = 100*obs/0.8 
 scale = (100-obs)/(100-control)
-experiments = lapply(expIDs, openDat, summaryFileE)
 
 plotExp <- function(r, nm) {
-    r = (r *scale)*100
+    r = r*100#(r *scale)*100
     plotStandardMap(r[[1]], cols = cols, limits = limits)
     mtext(side = 2, nm, line = -1.5)
     if (nm == names(experiments)[1]) mtext(side = 3, '10%')
@@ -53,11 +74,28 @@ plotExp <- function(r, nm) {
 
 
 png("figs/MortExcMap.png", height = 8, width = 7.2, res = 300, units = 'in')
+    experiments = lapply(expIDs, openDat, summaryFileE)
+
     layout(rbind(t(matrix(1:16, nrow = 2)), 17))
-    par(mar = c(0.5, 0, 0.5, 0), oma = c(0, 2, 2, 0))
+    par(mar = c(0.5, 0, 0.5, 0), oma = c(0, 3, 2, 0))
     mapply(plotExp, experiments, names(experiments))
     par(mar = rep(0, 4))
     addStandardLegend(obs, limits, cols, units = '%', srt = 0, add = FALSE,
                       plot_loc = c(0.2, 0.8, 0.73, 0.8), extend_max = TRUE) 
 dev.off()
 
+expIDs = c("Burnt\narea" = "noFire",
+           "Without\nrainfall\ndistribution" = "noDrought_noFire",
+           "Without\nhuman\nimpacts" = "noHumans_noFire")
+
+print("=====")
+limits = c(0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20)
+png("figs/FireMortMaps.png", height = 4, width = 7.2, res = 300, units = 'in')
+    experiments = lapply(expIDs, openDat, summaryFileE)
+    layout(rbind(t(matrix(1:6, nrow = 2)), 7))
+    par(mar = c(0.5, 0, 0.5, 0), oma = c(0, 4, 2, 0))
+    mapply(plotExp, experiments, names(experiments))
+    par(mar = rep(0, 4))
+    addStandardLegend(obs, limits, cols, units = '%', srt = 0, add = FALSE,
+                      plot_loc = c(0.2, 0.8, 0.73, 0.8), extend_max = TRUE) 
+dev.off()
